a7722c7a7ef736483219a880954de2eb2d17971f
rabius-sunny <rabiussunnyc3x@gmail.com>
2024-05-23 18:33:01 +0600

    chore

--- app/(routes)/dashboard/edit-profile/primary/page.tsx
+++ app/(routes)/dashboard/edit-profile/primary/page.tsx
@@ -1,11 +1,11 @@
 'use client'
 
 import React from 'react'
+import { PrimarySchema, TPrimary } from '@/constants/schema'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Button } from '@nextui-org/button'
 import { SubmitHandler, useForm } from 'react-hook-form'
 
-import { PrimarySchema, TPrimary } from '@/lib/schema'
 import CInput from '@/components/customs/CInput'
 import CSelect from '@/components/customs/CSelect'
 import PageHeader from '@/components/dashboard/PageHeader'
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,7 @@ const config: Config = {
   content: [
     './app/**/*.{js,ts,jsx,tsx,mdx}',
     './config/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
